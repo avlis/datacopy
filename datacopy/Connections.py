6756096714ae787473c893ba317df7442e9758ac
Bruno Silva <avlis.onurb@gmail.com>
2021-05-05 10:32:55 +0100

    wip

--- datacopy/Connections.py
+++ datacopy/Connections.py
@@ -1,5 +1,16 @@
 class Connection:
-    None
+    name = ''
+    driver = ''
+    server = ''	
+    database = ''
+    user = ''	
+    password = ''
+
+    valid_drivers=()
+
+    def testString(self):
+        if self.driver == '':
+            None
 
 class Connections:
     def __init__(self,p_confFileName):
