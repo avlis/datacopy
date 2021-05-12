c8c086940ff67fb151dbdfc8b3f51312b131d7b9
Bruno Silva <avlis.onurb@gmail.com>
2021-05-12 12:49:29 +0100

    some samples

---
+++ sample-query_m.sql
@@ -0,0 +1,4 @@
+select * 
+from one_source_table 
+where somefield='#A_VALUE#' and 
+    a_timestamp >'#DT_INI#' and a_timestamp<= '#DT_FIN#'
---
+++ sample-query_o.sql
@@ -0,0 +1,4 @@
+select * 
+from one_source_table 
+where somefield='#B_VALUE#' and 
+    a_timestamp between to_date("#DT_INI#", 'yyyymmdd') and to_date("#DT_FIN#", 'yyyymmdd')
