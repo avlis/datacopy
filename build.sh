d94ed17f1059e2b981a11e26e2ff1c2cbacff781
Bruno Silva <avlis.onurb@gmail.com>
2021-11-15 21:49:23 +0000

    add reminder

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 [ "$1" == "pull" ] && git pull
+if [ "$2" == "" ]; then
+        EXTRAVERSION=""
+else
+        EXTRAVERSION="-${2}"
+fi
 docker rmi localhost/datacopy
 rm -f /dev/shm/datacopy.tgz
-docker build --squash -t datacopy:latest . && docker save datacopy:latest -o /dev/shm/datacopy.tgz 
+docker build --squash -t datacopy:latest . && docker save datacopy${EXTRAVERSION}:latest -o /dev/shm/datacopy${EXTRAVERSION}.tgz 
+echo
+echo "DONE!"
+echo "now you can use:"
+echo  docker load < /dev/shm/datacopy${EXTRAVERSION}.tgz to update local images on other accounts."
