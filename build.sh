d5f2141d7aed5e98a3c66308e2c3b7b0cf6c41ce
Bruno Silva <avlis.onurb@gmail.com>
2023-10-15 12:03:18 +0100

    better build script, with args for proxies

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-[ "$1" == "pull" ] && git pull
+# note all args are passed to first build, practical for --build-arg HTTP_PROXY=...
 
-if [ "$2" == "" ]; then
-	EXTRAVERSION=""
-else
+if [ ! -z "${EXTRAVERSION}" ]; then
 	EXTRAVERSION="-${2}"
 fi
 
 if [ -z "${SKIPBASE}" ]; then
 	docker rm dcexport 2>&1 >/dev/null
 	docker rmi datacopy${EXTRAVERSION}:build datacopy${EXTRAVERSION}:flat
-	if 	docker build -t datacopy${EXTRAVERSION}:build -f Dockerfile.build . && \
+	if 	docker build -t datacopy${EXTRAVERSION}:build -f Dockerfile.build $* . && \
 		docker run --name dcexport datacopy${EXTRAVERSION}:build /bin/true && \
 		docker export dcexport | docker import - datacopy${EXTRAVERSION}:flat ; then
 		docker rm dcexport 2>&1 >/dev/null
